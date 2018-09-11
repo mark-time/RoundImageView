b50345bc5671177f35f32b2150623ca16ea261dc
mark_time <554801011@qq.com>
2018-09-11 11:58:46 +0800

    roundimageview-6

--- RoundImageView/app/src/main/java/com/guang/myapplication/MainActivity.java
+++ RoundImageView/app/src/main/java/com/guang/myapplication/MainActivity.java
@@ -1,17 +1,17 @@
-package com.guang.myapplication;
+//package com.guang.myapplication;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//
+//import com.guang.R;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.act_image_demo);
+//    }
 
-import com.guang.R;
-
-public class MainActivity extends AppCompatActivity {
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_image_demo);
-    }
-
-}
+//}
